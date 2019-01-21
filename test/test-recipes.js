52d46270105503cb2874ce45d51520def9982f31
Jacob Selbe <jalxselbe@gmail.com>
2019-01-20 18:18:46 -0600

    all recipes test functioning

--- test/test-recipes.js
+++ test/test-recipes.js
@@ -1,4 +1,4 @@
-const chai = require('chai'); //does this need to be double quotes?
+const chai = require('chai');
 const chaiHttp = require('chai-http');
 
 const {app, runServer, closeServer} = require('../server');
@@ -17,23 +17,23 @@ describe('Recipes', function() {
         return closeServer();
     });
     
-    it('should list items on GET', function() {
+    it('should list recipes on GET', function() {
         return chai
             .request(app)
             .get('/recipes')
             .then(function(res) {
                 expect(res).to.have.status(200);
                 expect(res).to.be.json;
-                expect(res).to.be.a('array');
+                expect(res.body).to.be.a('array');
                 expect(res.body.length).to.be.at.least(1);
                 res.body.forEach(function(item) {
                     expect(item).to.be.a('object');
-                    expect(item).to.include.keys('id', 'name', 'ingrdients');
+                    expect(item).to.include.keys('id', 'name', 'ingredients');
                 });
             });
     });
     
-    it('should list items on POST', function () {
+    it('should add recipes on POST', function () {
         const newRecipe = {name: "rice", ingredients: ["rice", "water"]};
         return chai
             .request(app)
@@ -43,7 +43,7 @@ describe('Recipes', function() {
                 expect(res).to.have.status(201);
                 expect(res).to.be.json;
                 expect(res).to.be.a('object');
-                expect(res).to.include.keys('id', 'name', 'ingredients');
+                expect(res.body).to.include.keys('id', 'name', 'ingredients');
                 expect(res.body.id).to.not.equal(null);
                 expect(res.body).to.deep.equal(
                     Object.assign(newRecipe, { id: res.body.id })
@@ -51,11 +51,37 @@ describe('Recipes', function() {
             });
     });
     
-    it('should list items on PUT', function () {
-
+    it("should update recipes on PUT", function () {
+        const updateData = {name: "rice", ingredients: ["rice", "water"]};
+        return (
+            chai
+                .request(app)
+                .get("/recipes")
+                .then(function (res) {
+                    updateData.id = res.body[0].id;
+                    return chai
+                        .request(app)
+                        .put(`/recipes/${updateData.id}`)
+                        .send(updateData);
+                })
+                .then(function (res) {
+                    expect(res).to.have.status(204);
+                    expect(res.body).to.be.a("object");
+                })
+        );
     });
-    
-    it('should list items on DELETE', function () {
 
+    it("should delete recipes on DELETE", function () {
+        return (
+            chai
+                .request(app)
+                .get("/recipes")
+                .then(function (res) {
+                    return chai.request(app).delete(`/recipes/${res.body[0].id}`);
+                })
+                .then(function (res) {
+                    expect(res).to.have.status(204);
+                })
+        );
     });
 });
